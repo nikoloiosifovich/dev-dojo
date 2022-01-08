82de8e0d2e67595341bbb02ecd4d4076f67d816c
Nikolo Iosifovich <vieiraneto88@gmail.com>
2022-01-08 19:41:49 -0300

    feat: thats a simples example of vars and primitive types

---
+++ programming-logic/VariablesAndPrimitiveTypes.java
@@ -0,0 +1,21 @@
+public class VariablesAndPrimitiveTypes {
+  public static void main(String[] args) {
+    byte userAgeLimit = 100;
+    short userLoan = 30000;
+    int userAge = 33;
+    long userPhone = 996089873;
+    float userHeight = 1.8f;
+    double userMoneyInWallet = 50.0;
+    char userGender = 'M';
+    boolean userActive = true;
+
+    System.out.println("Age Limit: " + userAgeLimit);
+    System.out.println("Loan: " + userLoan);
+    System.out.println("Age: " + userAge);
+    System.out.println("Phone: " + userPhone);
+    System.out.println("Height: " + userHeight);
+    System.out.println("Money: " + userMoneyInWallet);
+    System.out.println("Gender: " + userGender);
+    System.out.println("Active: " + userActive);
+  }
+}
