08ee9a13f01ada1a85474ffddfa0859d0acb8868
Nikolo Iosifovich <vieiraneto88@gmail.com>
2022-01-09 00:59:19 -0300

    feat: a literals class

---
+++ programming-logic/VariablesLiterals.java
@@ -0,0 +1,10 @@
+public class VariablesLiterals {
+    public static void main(String[] args) {
+        float salary = 2500.0f; // f for float
+        double extra = 300.5d; // d for double
+        long lightSpeed = 21002203238L; // L for long
+        int age = 20;
+
+        System.out.println(salary +" "+ extra +" "+ lightSpeed +" "+ age);
+    }
+}
