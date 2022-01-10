fc4b8c7c0cb4d45a9f04e757cc5137b8d2a79d8f
Nikolo Iosifovich <vieiraneto88@gmail.com>
2022-01-10 01:07:32 -0300

    feat: an example of char string boolean type

---
+++ programming-logic/CharStringBooleanVariables.java
@@ -0,0 +1,13 @@
+public class CharStringBooleanVariables {
+  public static void main(String[] args) {
+    char male = 'M';
+    char female = 'F';
+    char caractere = 65;
+    String name = "You can put text";
+    boolean condition = true;
+
+    System.out.println(caractere +" "+ male +" "+ female);
+    System.out.println(name);
+    System.out.println(condition);
+  }
+}
